fa7d0e516ef9609512b2414967e798cf8e96d087
dernovoj598 <dernovoj598@gmail.com>
2025-01-16 19:17:04 +0300

    Add files via upload

---
+++ greetigs.py
@@ -0,0 +1,13 @@
+username='Данила'
+title="Grade 1"
+content="Выполнение домашнего задание по Grade 1"
+status="Начало"
+created_date="13-01-2025"
+issue_date="14-01-2025"
+
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Описание:", content)
+print("Начало:", status)
+print("Дата создания:", created_date)
+print("Дедлайн:", issue_date)
